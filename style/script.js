dbe29741d2d5f053adb8821deee9da25b3dd5af4
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-08-27 18:08:45 +0200

    new site version

---
+++ style/script.js
@@ -0,0 +1,25 @@
+function responsiveTopBar() {
+var x = document.getElementById("Topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
+mybutton = document.getElementById("go-top-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
